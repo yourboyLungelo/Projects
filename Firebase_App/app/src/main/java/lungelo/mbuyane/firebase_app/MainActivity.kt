77cd11c104bd3b4a153b66944efe45f5c475902b
yourboyLungelo <mbuyanelungelo12@gmail.com>
2024-09-28 06:08:17 +0200

    Add files via upload

---
+++ Firebase_App/app/src/main/java/lungelo/mbuyane/firebase_app/MainActivity.kt
@@ -0,0 +1,46 @@
+package lungelo.mbuyane.firebase_app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import lungelo.mbuyane.firebase_app.ui.theme.Firebase_AppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Firebase_AppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    //code for app
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Firebase_AppTheme {
+        Greeting("Android")
+    }
+}
